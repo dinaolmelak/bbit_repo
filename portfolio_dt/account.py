55a782c2869b16761057d4c85fa90eabfb725780
Dinaol Tadesse <32272045+dinaolmelak@users.noreply.github.com>
2022-07-07 11:17:15 -0400

    algorithm of account.py

---
+++ portfolio_dt/account.py
@@ -0,0 +1,7 @@
+# create an account class to hold multiple positions and account name
+  # method getAccountName() return account name
+  # method getAllPositions() returns multiple positions
+  # method getPositionsForSecurity() 
+    # return map with query value
+  # method addPositions() add a set of position objects
+  # method removePosition() remove position from account with set of security names/security objects. ignore if not available
